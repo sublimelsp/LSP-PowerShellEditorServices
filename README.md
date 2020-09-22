5b34bfda2db83bb5c93a0cabcc25bb22e75577fd
Raoul Wols <raoulwols@gmail.com>
2020-09-22 20:17:31 +0200

    Bundle the server in the package (#2)

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # LSP-PowerShellEditorServices
 
-Convenience plugin to install and run the PowerShellEditorServices language server.
+Convenience plugin to run the PowerShellEditorServices language server.
 
 To use this package, install the [LSP](https://packagecontrol.io/packages/LSP) 
 package, and the [PowerShell](https://packagecontrol.io/packages/PowerShell) package
 for syntax highlighting. Alternatively, you may use Michael Blyons'
 [PowerShell syntax rewrite](https://github.com/michaelblyons/PowerShell/tree/sublime-syntax).
-
-This package will download binaries in Sublime Text's `$DATA/Cache` directory.
---
+++ make.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+function set-output
+{
+    local value="${2//'%'/'%25'}"
+    local value="${value//$'\n'/'%0A'}"
+    echo "::set-output $1::${value//$'\r'/'%0D'}"
+}
+
+function main
+{
+    local pwsh=PowerShellEditorServices
+    rm -rf out
+    rm -rf $pwsh
+    rm -f $pwsh.zip
+    rm -f LSP-$pwsh.zip
+    mkdir -p out
+    cp -R src/* out/
+    cp LICENSE out/
+    cp NOTICE out/
+    touch out/.no-sublime-package
+    curl -L -s -S >/dev/null https://github.com/PowerShell/$pwsh/releases/download/$1/$pwsh.zip -o $pwsh.zip
+    unzip -qq $pwsh.zip
+    mv $pwsh/* out/
+    rmdir $pwsh
+    rm $pwsh.zip
+    pushd out && zip -qq -r LSP-$pwsh.zip . && popd
+    mv out/LSP-$pwsh.zip .
+    rm -rf out
+    set-output artifact LSP-$pwsh.zip
+}
+
+main "$@"
