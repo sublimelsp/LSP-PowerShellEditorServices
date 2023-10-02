9a5e08f5deb67213988ad51d0d5d189f4f8cc6ed
deathaxe <deathaxe82@googlemail.com>
2023-10-02 12:15:12 +0200

    Reorganize public API methods
    This commit moves and groups all public API hooks at the beginning of the class.

--- plugin.py
+++ plugin.py
@@ -17,9 +17,53 @@ URL = "https://github.com/PowerShell/PowerShellEditorServices/releases/download/
 
 
 class PowerShellEditorServices(AbstractPlugin):
+
+    # ---- public API methods ----
+
     @classmethod
     def name(cls) -> str:
         return cls.__name__
+
+    @classmethod
+    def needs_update_or_installation(cls) -> bool:
+        try:
+            cmd = '[System.Diagnostics.FileVersionInfo]::GetVersionInfo("{}").FileVersion'.format(cls.dll_path())
+            version_info = cls.run(cls.powershell_exe(), "-Command", cmd).decode('ascii')
+            version_info = ".".join(version_info.splitlines()[0].strip().split('.')[0:3])
+            return cls.version_str() != version_info
+        except Exception:
+            pass
+        return True
+
+    @classmethod
+    def install_or_update(cls) -> None:
+        shutil.rmtree(cls.basedir(), ignore_errors=True)
+        os.makedirs(cls.storage_path(), exist_ok=True)
+        try:
+            zipfile = os.path.join(cls.storage_path(), "{}.zip".format(cls.name()))
+            urlretrieve(URL.format(cls.version_str()), zipfile)
+            with ZipFile(zipfile, "r") as f:
+                f.extractall(cls.basedir())
+            os.unlink(zipfile)
+        except Exception:
+            shutil.rmtree(cls.basedir(), ignore_errors=True)
+            raise
+
+    @classmethod
+    def can_start(cls, window: sublime.Window, initiating_view: sublime.View,
+                  workspace_folders: List[WorkspaceFolder], configuration: ClientConfig) -> Optional[str]:
+        if not configuration.command:
+            if sublime.platform() == "windows":
+                configuration.command = cls.get_windows_command()
+            else:
+                configuration.command = cls.get_unix_command()
+
+        return super().can_start(window, initiating_view, workspace_folders, configuration)
+
+    def m_powerShell_executionStatusChanged(self, params: Any) -> None:
+        pass
+
+    # ---- internal methods -----
 
     @classmethod
     def get_windows_command(cls) -> List[str]:
@@ -128,42 +172,3 @@ class PowerShellEditorServices(AbstractPlugin):
         else:
             startupinfo = None
         return subprocess.check_output(args=args, cwd=kwargs.get("cwd"), startupinfo=startupinfo)
-
-    @classmethod
-    def needs_update_or_installation(cls) -> bool:
-        try:
-            cmd = '[System.Diagnostics.FileVersionInfo]::GetVersionInfo("{}").FileVersion'.format(cls.dll_path())
-            version_info = cls.run(cls.powershell_exe(), "-Command", cmd).decode('ascii')
-            version_info = ".".join(version_info.splitlines()[0].strip().split('.')[0:3])
-            return cls.version_str() != version_info
-        except Exception:
-            pass
-        return True
-
-    @classmethod
-    def install_or_update(cls) -> None:
-        shutil.rmtree(cls.basedir(), ignore_errors=True)
-        os.makedirs(cls.storage_path(), exist_ok=True)
-        try:
-            zipfile = os.path.join(cls.storage_path(), "{}.zip".format(cls.name()))
-            urlretrieve(URL.format(cls.version_str()), zipfile)
-            with ZipFile(zipfile, "r") as f:
-                f.extractall(cls.basedir())
-            os.unlink(zipfile)
-        except Exception:
-            shutil.rmtree(cls.basedir(), ignore_errors=True)
-            raise
-
-    @classmethod
-    def can_start(cls, window: sublime.Window, initiating_view: sublime.View,
-                  workspace_folders: List[WorkspaceFolder], configuration: ClientConfig) -> Optional[str]:
-        if not configuration.command:
-            if sublime.platform() == "windows":
-                configuration.command = cls.get_windows_command()
-            else:
-                configuration.command = cls.get_unix_command()
-
-        return super().can_start(window, initiating_view, workspace_folders, configuration)
-
-    def m_powerShell_executionStatusChanged(self, params: Any) -> None:
-        pass
